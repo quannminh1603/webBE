85ace0fe8fa31a6d7958346a6644e375150f911a
quannminh1603 <quannminh1603@gmail.com>
2023-12-21 09:17:21 +0700

    final website

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -1,29 +1,3 @@
-// const mongoose = require('mongoose')
-
-// const productSchema = new mongoose.Schema(
-//     {
-//         tenSanPham: {type: String, required: true},
-//         donGia: {type: Number, required: true},
-//         soLuongConLai: {type: Number, required: true},
-//         size: {type: Number, required: true},
-//         type: {type: String, required: true},
-//         hinhAnh: {type: String, required: true},
-//         rating: {type: Number, required: true},
-//         description: {type: String, required: true},
-//         discount: { type: Number },
-//         mount: { type: Number },
-//         selled: { type: Number }
-//     },
-//     {
-//         timestamps: true
-//     }
-// );
-
-// const Product = mongoose.model("Product", productSchema);
-// module.exports = Product;
-
-
-// test
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema(
